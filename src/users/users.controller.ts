99f4b249032b42587692ef4f787522de5c076653
Alejo <20245281@esen.edu.sv>
2026-05-14 07:38:00 -0600

    modulo, controlador y servicio

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+
+  @Post()
+  createUsers(@Body() userData: { name: string }) {
+    return this.usersService.createUsers(userData.name);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProvider';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+    { id: 1, name: 'John Doe'},
+    { id: 2, name: 'Jane Doe'},
+    { id: 3, name: 'Jim Doe'},
+    { id: 4, name: 'Jack Doe'},
+    { id: 5, name: 'Jill Doe'},
+    ];
+
+    getUsers() {
+        return this.users;
+    }
+
+    createUsers(name: string) {
+        const newUser = { id: Date.now(), name };
+        this.users.push(newUser);
+        return newUser;
+    }
+}
